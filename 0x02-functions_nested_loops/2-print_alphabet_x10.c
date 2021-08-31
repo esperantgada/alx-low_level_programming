8a8f8ef95163901166a4ec278e6b9bc008d3c7a7
Espérant GADA <esperantgada@gmail.com>
2021-08-31 15:13:48 -0700

    Print alphabet in lowercase ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_alphabet - Print alphabet in lowercase
+ *
+ *Return: no return
+*/
+
+void print_alphabet(void)
+{
+	char alphabet;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchar('\n');
+	}
+
+	return (0);
+}
