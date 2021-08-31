539b3643936687b3a0399ef265e4154a685e8f87
Espérant GADA <esperantgada@gmail.com>
2021-08-31 14:53:13 -0700

    Print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_alphabet - Print alphabet in lowercase
+ *
+ *Return: no return
+*/
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet < 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+{
