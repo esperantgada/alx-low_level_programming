a7696ed967dd0316e943eb61772de8b7cbdaf3e3
Espérant GADA <esperantgada@gmail.com>
2021-09-01 10:02:26 -0700

    Print a list of number to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_to_98 - prints natural
+*numbers from n to 98
+*@n:integer to start by
+*return: returns nothing
+*/
+void print_to_98(int n)
+{
+	int m;
+
+	if (n <= 98)
+	{
+		for (m = n; m <= 98; m++)
+		{
+			printf("%d, ", m);
+		{
+		printtf("\n");
+
+	}
+
+	else
+	{
+		for (m = n; m <= 98; m++)
+		{
+			printf("%d, ", m);
+		{
+		printf("\n");
+	}
+}
