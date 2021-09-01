4f31ade417b2d614535012d03648c3a183ddd825
Espérant GADA <esperantgada@gmail.com>
2021-08-31 18:09:18 -0700

    Check letter in lower or upper case

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-*_isalpha - controls if a character is alphabetical
+*_isalpha - check if a character is alphabetical
 *@c: character to be verified
 *Return: return 0 or 1
 */
 int _isalpha(int c)
 {
-if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+if ((c >= '65n' && c <= '90') || (c >= '97' && c <= '122'))
 return (1);
 
 return (0);
