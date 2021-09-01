7a934d3c884c32df7f4074ea085a3c8589f35e7b
Espérant GADA <esperantgada@gmail.com>
2021-09-01 06:42:58 -0700

    Update code

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,19 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- *print_alphabet - Print alphabet in lowercase
- *print ten times
- *desc: The function uses nested for loop
- *Return: no return
+*print_alphabet_x10 - print 10 times the alphabet, in lowercase.
+*
+*Return: Always 0.
 */
-
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int j, i;
 
-	for (i = 0; i < 10; i++)
+	for (j = 1; j <= 10; j++)
 	{
-		for (j = 'a'; j <= 'z'; j++)
+		for (i = 'a'; i <= 'z'; i++)
 		{
-			_putchar(j);
+			_putchar(i);
 		}
 		_putchar('\n');
 	}
-
 }
