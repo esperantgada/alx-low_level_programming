1ddbd3fba5dd38441a2fc5f3c536c15ce55ce20a
Espérant GADA <esperantgada@gmail.com>
2021-08-31 17:28:13 -0700

    Print alphabet in lowercase ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,10 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
  *print_alphabet - Print alphabet in lowercase
  *print te times
  *desc: The function uses nested for loop
-=======
  *print_alphabet - Print alphabet in lowercase ten times
- *
->>>>>>> 63af7cf48ee528a5cf93b0aa6f74d1e29b41e599
  *Return: no return
 */
 
