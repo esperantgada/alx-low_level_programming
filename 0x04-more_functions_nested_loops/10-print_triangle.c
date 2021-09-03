12ae3e5f09c5f737e5760fe90cbb384694585422
Espérant GADA <esperantgada@gmail.com>
2021-09-03 02:09:23 -0700

    Add function that draws triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *print_triangle - draws a triangle
+ *@size: value
+ *Return: no return value
+*/
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (k = 1; k <= (size - i); k++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
