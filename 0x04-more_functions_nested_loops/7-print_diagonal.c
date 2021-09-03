71f432e019d6448b5654c36e2dcd74781a10d6e1
Espérant GADA <esperantgada@gmail.com>
2021-09-03 01:20:24 -0700

    Add conditions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,19 +10,27 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 1; i <= n; i++)
+	if (n <= 0)
 	{
-		for (j = 1; j <= n; j++)
-		{
-			if (i == j)
-			{
-				_putchar('\\');
-			}
-			else
-			{
-				_putchar(' ');
-			}
-		}
 		_putchar('\n');
 	}
+	else
+	{
+		 for (i = 1; i <= n; i++)
+		 {
+			 for (j = 1; j <= n; j++)
+			 {
+				 if (i == j)
+				 {
+					 _putchar('\\');
+				 }
+				 else
+				 {
+					 _putchar(' ');
+				 }
+			 }
+			 _putchar('\n');
+		 }
+	}
+
 }
