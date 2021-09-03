deeceb2631d66d7b3916872582ac4ff0912c583a
Espérant GADA <esperantgada@gmail.com>
2021-09-03 00:44:31 -0700

    Add new function

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_diagonal - a function that draws a diagonal
+ *
+ *Return: no retur value
+*/
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if (i == j)
+			{
+				_putchar('\\');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
