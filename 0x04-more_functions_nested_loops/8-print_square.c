8503bddef395453d2674c86c3ec61e03d9f549b1
Espérant GADA <esperantgada@gmail.com>
2021-09-03 01:40:08 -0700

    Add function for square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *print_square - draws a square with # sign
+ *@size:value
+ *Return: no return value
+*/
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
