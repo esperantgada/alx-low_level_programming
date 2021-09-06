d0e14d364c77abb539c5b3ddd896a180489efa71
Espérant GADA <esperantgada@gmail.com>
2021-09-06 13:33:43 -0700

    Add new function

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+*puts_half - puts half of a string
+*@str: string
+*Return:void
+*/
+void puts_half(char *str)
+{
+	int len = _strlen(str);
+
+	if (len % 2 == 0)
+	{
+		len = len / 2;
+	}
+
+	else
+	{
+		len = (len + 1) / 2;
+	}
+
+	while (str[len] != '\0')
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	_putchar('\n');
+}
+
+
+/**
+ * _strlen - return the length of the string
+ *@s: char type pointer
+ *Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i, compt = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		compt++;
+	}
+	return (compt);
+
+}
