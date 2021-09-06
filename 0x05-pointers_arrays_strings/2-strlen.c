6ae29f4cbfd03f0a2a7a6a1f3ec36ef1bb0e6bfa
Espérant GADA <esperantgada@gmail.com>
2021-09-06 09:16:15 -0700

    Add string's length function

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
  * _strlen - return the length of the string
- * @s: char type pointer
- * Return: length of string
+ *@s: char type pointer
+ *Return: length of string
  */
 int _strlen(char *s)
 {
-	int i;
+	int i, compt = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		return (i);
+		compt++;
 	}
+
+	return (compt);
 
 }
