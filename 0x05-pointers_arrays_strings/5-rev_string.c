b1aa07b98ff74fe20084b089bb33ad441060b06c
Espérant GADA <esperantgada@gmail.com>
2021-09-07 03:39:19 -0700

    Add new changes

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,55 @@
+#include "main.h"
+/**
+*rev_string - Reverse a string
+*@s: String to reverse
+*Return: no return value
+*/
+void rev_string(char *s)
+{
+	int i, l;
+
+	l = _strlen(s) - 1;
+
+	while (s[i] != '\0')
+	{
+		swap_char(s + l, s + i);
+		i++;
+		l--;
+
+	}
+}
+
+
+/**
+ * _strlen - return the length of the string
+ *@s: char type pointer
+ *Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i, compt = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		compt++;
+	}
+	return (compt);
+
+}
+
+
+
+
+/**
+ * swap_int - swap two characters
+ * @a: char pointer type
+ * @b: char pointer type
+ */
+void swap_char(char *a, char *b)
+{
+        char c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
