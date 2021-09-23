80d3230203c30a838caaea92a5f738596c835e46
Esperant GADA <esperantgada.com>
2021-09-23 12:20:18 -0700

    Remove whitespace

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,13 +16,13 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	
+
 	l = max - min + 1;
 	a = malloc(sizeof(int) * l);
-	
+
 	if (a == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i < l; i++, min++)
 	{
 		a[i] = min;
