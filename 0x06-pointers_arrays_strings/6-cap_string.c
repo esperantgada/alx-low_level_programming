3433bbb66053d489c2746dee1d3764912f3e1676
Esperant GADA <esperantgada.com>
2021-10-12 13:32:56 -0700

    Review code

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,30 @@
 #include "main.h"
+
 /**
-*cap_string - function that capitalize first character of a word
-*@str: string to capitalize
-*Return:returns the capitalized string
-*/
+* cap_string - capitalizes all words of a string
+* @str: string
+* Return: a string
+**/
 char *cap_string(char *str)
 {
-	int i = 0;
+	int i = 0, j;
+	char sim[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (str[++i])
+	while (str[i] != '\0')
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
-
-		if (str[i - 1] == ' ' ||
-				str[i - 1] == '\t' ||
-				str[i - 1] == '\n' ||
-				str[i - 1] == ',' ||
-				str[i - 1] == ';' ||
-				str[i - 1] == '.' ||
-				str[i - 1] == '!' ||
-				str[i - 1] == '?' ||
-				str[i - 1] == '"' ||
-				str[i - 1] == '(' ||
-				str[i - 1] == ')' ||
-				str[i - 1] == '{' ||
-				str[i - 1] == '}')
-			str[i] -= 32;
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			for (j = 0; j <= 12; j++)
+			{
+				if (str[i - 1] == sim[j])
+				{
+					str[i] = str[i] - 32;
+				}
+			}
+		}
+		i++;
 	}
+	if (str[0] >= 97 && str[0] <= 122)
+		str[0] = str[0] - 32;
 	return (str);
 }
